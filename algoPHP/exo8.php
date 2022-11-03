eaa7d684467a9ac909f61f2fb058084bd766208a
Freaky Fennec <ahjinani@gmail.com>
2022-11-03 08:59:20 +0100

    Ajouté exo8.php

---
+++ algoPHP/exo8.php
@@ -0,0 +1,18 @@
+<!--
+    exo8.php
+=============================================
+    By AHJ
+-->
+
+<h1>Exercice 8</h1>
+
+<p></p>
+
+<h2>Résultat</h2>
+
+<?php
+// variables de travail
+
+
+echo "<br>";
+?>
