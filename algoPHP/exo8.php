b1d155af86493fe48b0e6b8a5163a43b4f504767
Freaky Fennec <ahjinani@gmail.com>
2022-11-03 09:54:39 +0100

    finished exo8.php

--- algoPHP/exo8.php
+++ algoPHP/exo8.php
@@ -16,11 +16,19 @@ $nbrDeLaTable = 8;
 $longueurDeLaTable = 10;
 
 // boucle pour la table de multiplication (boucle for)
+echo "Boucle for()<br>";
 for ($i=1; $i <= $longueurDeLaTable; $i++) { 
     $resultMultiplication = $i * $nbrDeLaTable;
     echo "$i x $nbrDeLaTable = $resultMultiplication<br>";
 }
 
+
 // boucle pour la table de multiplication (boucle while)
-for($i in )
+echo "Boucle while()<br>";
+$j = 1;
+while($j <= 10) {
+    $resultMultiplication = $j * $nbrDeLaTable;
+    echo "$j x $nbrDeLaTable = $resultMultiplication<br>";    
+    $j++;
+}
 ?>
