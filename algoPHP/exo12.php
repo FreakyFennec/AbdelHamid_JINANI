ad4394b39b06e6bb3768de27f01515161d84c97e
FreakyFennec <ahjinani@gmail.com>
2022-11-04 18:14:21 +0100

    ??

--- algoPHP/exo12.php
+++ algoPHP/exo12.php
@@ -6,7 +6,8 @@
 
 <h1>Exercice 12</h1>
 
-<p></p>
+<p>A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée (tableau  contenant  clé  et  valeur),  dire  bonjour  aux  différentes  personnes  dans  leur  langue respective (français ➔«Salut», anglais ➔«Hello», espagnol ➔«Hola»)<br><br>
+Exemple: tableau ➔Mickaël -> FRA, Virgile -> ESP, Marie-Claire -> ENG</p>
 
 <h2>Résultat</h2>
 
