eea7fe0105053f4ddb2bf3fabebfd300c5d5bf55
FreakyFennec <ahjinani@gmail.com>
2022-11-06 17:35:17 +0100

    Modified exo15.php

--- algoPHP/exo15.php
+++ algoPHP/exo15.php
@@ -1,48 +1,59 @@
-<!--
-    exo15.php
-=============================================
-    By AHJ
--->
-
-<h1>Exercice 15</h1>
-
-<p>Créer une classe Personne (nom, prénom et date de naissance).<br>
-Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.<br><br>
-$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;<br>
-$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
-
-<h2>Résultat</h2>
-
 <?php
-/*
-declare(strict_type = 1);
-*/
+
+// declare(strict_type = 1);
+
+//<!--
+    //exo15.php
+//=============================================
+    //By AHJ
+//-->
+
+echo "<h1>Exercice 15</h1>
+
+    <p>Créer une classe Personne (nom, prénom et date de naissance).<br>
+    Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.<br><br>
+
+    <h2>Résultat</h2>";
+
+
+
 class Personnes {
     public $name;
     public $nickName;
     public $birth;
 
-    // fonction calcul age
-    public function displayIdentity($name, $nickName, $birth) {
-        // variable de travail
-        $currentDate = new DateTime(); // date d'aujourd'hui
-        // calculs
-        $interval = $currentDate->diff($birth); // objet (?) contenant les années, les mois, les jours etc...
+    // Calcul age
+    public function displayIdentity() {
+        /*
+        // Variable de travail
+        $currentDate = new DateTime(); // Date d'aujourd'hui
+        // Calculs
+        $interval = $currentDate->diff($birth); // Objet (?) contenant les années, les mois, les jours etc...
 
-        $age = $interval->y; // extraction du nombre d'années
-
-        echo "$nickName $name a $age <br>";
+        $age = $interval->y; // Extraction du nombre d'années
+*/
+        echo "$this->nickName $this->name a $this->age ans.<br>";
     }   
 }
 
 /**/
-$p1 = new Personnes("DUPONT", "Michel", "1980-02-19");
+$p1 = new Personnes("DUPONT", "Michel", "1980-02-19"); // Instantiation (création objet $p1)
 
-$p2 = new Personnes("DUCHEMIN", "Alice", "1982-01-17");
+$p2 = new Personnes("DUCHEMIN", "Alice", "1982-01-17"); // Instantiation (création objet $p2)
 
 /**/
-var_dump($p1); echo "<br>";
-var_dump($p2);
-  
+$p1 = new Personnes(); // Objet
+$p1->name = "DUPONT";
+$p1->nickName = "Michel";
+$p1->birth = "1980-02-19";
+
+$p2 = new Personnes(); // Objet
+$p2->name = "DUCHEMIN";
+$p2->nickName = "Alice";
+$p2->birth = "1982-01-17";
+/**/
+
+$p1->displayIdentity();
+$p2->displayIdentity();
 
 ?>
