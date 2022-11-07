d435dc0369a036a8ae5b37ec524df7d0e54bf771
FreakyFennec <ahjinani@gmail.com>
2022-11-07 07:04:45 +0100

    Try to declare strict_type

--- algoPHP/exo15.php
+++ algoPHP/exo15.php
@@ -8,49 +8,45 @@
     //By AHJ
 //-->
 
-echo "<h1>Exercice 15</h1>
+    echo "<h1>Exercice 15</h1>
 
     <p>Créer une classe Personne (nom, prénom et date de naissance).<br>
     Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.<br><br>
+    (dollar)p1 = new Personne('DUPONT', 'Michel', '1980-02-19') ;<br>
+    (dollar)p2 = new Personne('DUCHEMIN', 'Alice', '1985-01-17') ;</p>
 
     <h2>Résultat</h2>";
 
+    class Personnes {
+        public $name;
+        public $nickName;
+        public $birth;
 
+        public function __construct($nickName , $name , $birth) {
+            $this->name = $name;
+            $this->nickName = $nickName;
+            $this->birth = $birth;
+        }
 
-class Personnes {
-    public $name;
-    public $nickName;
-    public $birth;
+        // Calcul age
+        public function displayIdentity() {
+            /*
+            // Variable de travail
+            $currentDate = new DateTime(); // Date d'aujourd'hui
+            // Calculs
+            $interval = $currentDate->diff($birth); // Objet (?) contenant les années, les mois, les jours etc...
 
-    public function __construct($nickName , $name , $birth) {
-        $this->name = $name;
-        $this->nickName = $nickName;
-        $this->birth = $birth;
+            $age = $interval->y; // Extraction du nombre d'années
+    */
+            echo "$this->nickName $this->name a $this->birth ans.<br>";
+        }   
     }
 
-    // Calcul age
-    public function displayIdentity() {
-        /*
-        // Variable de travail
-        $currentDate = new DateTime(); // Date d'aujourd'hui
-        // Calculs
-        $interval = $currentDate->diff($birth); // Objet (?) contenant les années, les mois, les jours etc...
+    $p1 = new Personnes("DUPONT", "Michel", "1980-02-19"); // Instantiation (création objet $p1)
 
-        $age = $interval->y; // Extraction du nombre d'années
-*/
-        echo "$this->nickName $this->name a $this->birth ans.<br>";
-    }   
-}
+    $p2 = new Personnes("DUCHEMIN", "Alice", "1982-01-17"); // Instantiation (création objet $p2)
 
-/**/
-$p1 = new Personnes("DUPONT", "Michel", "1980-02-19"); // Instantiation (création objet $p1)
-
-$p2 = new Personnes("DUCHEMIN", "Alice", "1982-01-17"); // Instantiation (création objet $p2)
-
-
-/**/
-
-$p1->displayIdentity();
-$p2->displayIdentity();
+    $p1->displayIdentity();
+    $p2->displayIdentity();
 
 ?>
