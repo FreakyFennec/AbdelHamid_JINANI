35a519eab477f8fc165f8ed8e1506ec4f7f684ca
SelcukYalcin <yalcinselcuk79@gmail.com>
2022-11-14 16:59:50 +0100

    created file author

--- algoPHP_POO_02/algoPHP_POO_02_class_author.php
+++ algoPHP_POO_02/algoPHP_POO_02_class_author.php
@@ -10,9 +10,32 @@
 Une méthode toString() sera appréciée dans chacune de vos classes.<br>
 Vous implémenterez une méthode afficherBibliographie() qui permettra d’afficher la bibliographie complète d’un auteur :</p>
 
-<?php
+<?php  
+class Author {
+    protected $_surName;
+    protected $_firstName;
+    protected $_biblio;
 
-    // variables de travail
+   
+    public function __construct($_surName, $_firstName) {
+        $this->_surName = $_surName;
+        $this->_firstName = $_firstName;
+    }
+
+    public function __toString() {
+        
+    }
+
+    public function getSurName() {
+        return $this->_surName;
+    }
+
+    public function setSurName() {
+        return $this->_surName;
+    }
+
+
+}
     
 
 
