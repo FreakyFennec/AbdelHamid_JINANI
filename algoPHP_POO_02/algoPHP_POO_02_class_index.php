6f968c9cec84fa15316cb1448aed77a15cf0da7e
SelcukYalcin <yalcinselcuk79@gmail.com>
2022-11-14 16:33:19 +0100

    created files books management

---
+++ algoPHP_POO_02/algoPHP_POO_02_class_index.php
@@ -0,0 +1,19 @@
+<!--
+    algoPHP_POO_02_class_index.php
+    =============================================
+    By AHJ
+-->
+
+<h1>algoPHP_POO_02_class_index.php</h1>
+
+<p>Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un auteur. Un auteur comporte un nom et un prénom.<br>
+Une méthode toString() sera appréciée dans chacune de vos classes.<br>
+Vous implémenterez une méthode afficherBibliographie() qui permettra d’afficher la bibliographie complète d’un auteur :</p>
+
+<?php
+
+    // variables de travail
+    
+
+
+?>
