2d1fbbca8e8913bcea9437a355df78277c73f201
FreakyFennec <ahjinani@gmail.com>
2022-11-16 11:11:29 +0100

    added array biblio

--- algoPHP_POO_02/Stephen KING/algoPHP_POO_02_class_index.php
+++ algoPHP_POO_02/Stephen KING/algoPHP_POO_02_class_index.php
@@ -20,6 +20,36 @@
 
         $author = new Authors("Stephen", "KING");
 
+        $biblioStephenKing = [
+            [
+            "title" => "Ca",
+            "publicationDate" => "1986",
+            "pagesQuant" => "1138",
+            "price" => "20"
+            ],
+
+            [
+            "title" => "Simetierre",
+            "publicationDate" => "1983",
+            "pagesQuant" => "374",
+            "price" => "15"
+            ],
+
+            [
+            "title" => "Le Fléau",
+            "publicationDate" => "1978",
+            "pagesQuant" => "823",
+            "price" => "14"
+            ],
+
+            [
+            "title" => "Shining",
+            "publicationDate" => "1977",
+            "pagesQuant" => "447",
+            "price" => "16"
+            ],
+        ];
+
         $bibliography = new Bibliographys ();
         // var_dump($bibliography);
     ?>
