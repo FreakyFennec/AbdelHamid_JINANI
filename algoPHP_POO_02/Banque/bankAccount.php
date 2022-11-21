d3e946e5ce194dc6ea0ed7c7b7bd093c91967966
abdelHamid_JINANI <ahjinani@gmail.com>
2022-11-21 11:24:10 +0100

    feed bankAccount

--- algoPHP_POO_02/Banque/bankAccount.php
+++ algoPHP_POO_02/Banque/bankAccount.php
@@ -1,10 +1,63 @@
 <?php
-    class comptBanquaire {
-
+    class compteBanquaire 
+    {
         private string $nomCompte;
-        private string $;
-        private string $;
-        private string $;
-        private string $;
+        private string $soldeInitial;
+        private string $devise;
+        private string $titulaireCompte;
+    
+        // Constructeur
+        public function __construct(string $nomCompte, int $soldeInitial, string $devise, string $titulaireCompte) 
+        {        
+            $this->nomCompte = $nomCompte;
+            $this->soldeInitial = $soldeInitial;
+            $this->devise = $devise;
+            $this->titulaireCompte = $titulaireCompte;
+            $this->titulaireCompte->ajouterCompte($this);
+        }
+
+        // Set and get nomCompte
+        public function getnomCompte():string
+        {
+            return $this->nomCompte;
+        }       
+        public function setnomCompte($nomCompte)
+        {
+            $this->nomCompte = $nomCompte;
+            return $this;
+        }
+
+        // Set and get soldeInitial
+        public function getsoldeInitial():string
+        {
+            return $this->soldeInitial;
+        }       
+        public function setsoldeInitial($soldeInitial)
+        {
+            $this->soldeInitial = $soldeInitial;
+            return $this;
+        }
+
+        // Set and get devise
+        public function getdevise():string
+        {
+            return $this->devise;
+        }       
+        public function setdevise($devise)
+        {
+            $this->devise = $devise;
+            return $this;
+        }
+
+        // Set and get titulaireCompte
+        public function gettitulaireCompte():string
+        {
+            return $this->titulaireCompte;
+        }       
+        public function settitulaireCompte($titulaireCompte)
+        {
+            $this->titulaireCompte = $titulaireCompte;
+            return $this;
+        }
     }
 ?>
