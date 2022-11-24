733a05de810739236d6de4b7abf2cb3ba2bb6a01
Hamid-JINANI <ahjinani@gmail.com>
2022-11-24 14:06:07 +0100

    display director and actor

--- algoPHP_POO_02/Cinema/Person.php
+++ algoPHP_POO_02/Cinema/Person.php
@@ -7,14 +7,16 @@
         private string $gender;
         private DateTime $birth;
         private array $movies;
+        private string $contribution;
 
         // Constructor
-        public function __construct(string $surName, string $firstName, string $gender,  string $birth) {
+        public function __construct(string $surName, string $firstName, string $gender,  string $birth, string $contribution) {
             $this->surName = $surName;
             $this->firstName = $firstName;
             $this->gender = $gender;
             $this->birth = new DateTime($birth);
             $this->movies = [];
+            $this->contribution = $contribution;
         }
 
         // Get and Set surName
@@ -62,9 +64,41 @@
             return $this;
         }
 
+        // Get and Set contribution
+        public function getcontribution() {
+           return $this->contribution;
+        }
+         public function setcontribution($contribution) {
+           $this->contribution = $contribution;
+           return $this;
+        }
+
         // Method display age
         public function getAge() {
             return date_diff(new DateTime(), $this->birth)->format("%Y"); // différence(dateActuelle, dateNaissance)->formatée(années).
         }
+
+        // Method add movies
+        public function addMovie(Movie $movie) {
+            $this->movies[] = $movie;
+        }
+
+        // Method add genre
+        public function addGenre(Genre $genre) {
+            $this->$genre;
+            return $this;
+        }
+
+         // method display infos director
+         public function displayInfosPerson() {
+            return "
+                <div>
+                    <p>Surname : " . $this->surName . "</p>
+                    <p>First name : " . $this->firstName . "</p>
+                    <p>Age : " . $this->getAge() . " ans.</p>
+                    <p>Contribution : " . $this->contribution . "</p>
+                </div>";
+        }
+
     }
 ?>
--- algoPHP_POO_02/Cinema/index.php
+++ algoPHP_POO_02/Cinema/index.php
@@ -16,13 +16,23 @@
 
         $sciFi = new Genre("Science Fiction");
 
-        $georgeLucas = new Director ("Lucas", "George", "Man", "1944-05-14");
+        $georgeLucas = new Director ("George", "Lucas", "Man", "1944-05-14");
 
         echo $georgeLucas->displayInfosDirector();
 
         $movie = new Movie ("Star Wars Episode IV", $georgeLucas, 1977, 121, $sciFi, "Il y a bien longtemps, dans une galaxie très lointaine... La guerre civile fait rage entre l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans de l'Etoile Noire, une station spatiale invulnérable, à son droïde R2-D2 avec pour mission de les remettre au Jedi Obi-Wan Kenobi. (AlloCiné).");
 
         echo $georgeLucas->displayMovies();
+
+
+        $georgeLucas = new Person ("George", "Lucas", "Man", "1944-05-14", "Director");
+
+        echo $georgeLucas->displayInfosPerson();
+
+
+        $harrisonFord = new Person ("Ford", "Harrison", "Man", "1942-07-13", "Actor");
+
+        echo $harrisonFord->displayInfosPerson();
     ?>
 </body>
 </html>
