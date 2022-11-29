b0c95c177b2ee86c03c0b50f1e43064544cb1637
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-11-29 08:22:02 +0100

    session_save_path()

--- algoPHP_POO_02/appli/index.php
+++ algoPHP_POO_02/appli/index.php
@@ -1,3 +1,10 @@
+<?php
+    session_save_path();        // Chemin où sont sauvegardé les données. (faire un echo pour visualiser)
+    session_start();
+    echo "Salut";
+    $_SESSION["user"] = "Freaky";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
