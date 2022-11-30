c2f95c18a7bf38aef028e0aa0e7fb837a322f28a
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-11-30 11:31:58 +0100

    toujours pas de traitement

--- algoPHP_POO_02/appli/recap.php
+++ algoPHP_POO_02/appli/recap.php
@@ -23,7 +23,9 @@
         </nav>
         <main>
         <?php
-            if(isset($_GET["action"])) {
+            if(!isset($_SESSION["products"]) || empty($_SESSION["products"])) {
+                echo "<p>Aucun produit en session...</p>";
+            } else {
                 echo "<table>",
                         "<thead>",
                             "<tr>",
@@ -36,75 +38,24 @@
                             "</tr>",
                         "</thead>",
                         "<tbody>";
-
-
-                switch($_GET["action"]) {
-
-                    // AJOUTER UN PRODUIT
-                    case "add":
-
-                        if(isset($_POST["submit"])) {
-                            // Ici on vérifie l'intégrité des valeurs transmises dans le tableau $_POST.
-                            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                            $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-                            $qqt = filter_input(INPUT_POST, "qqt", FILTER_VALIDATE_INT);
-                        }
-
-
-                        
-
-                        header("Location: recap.php");
-                        break;
-
-                        
-
-                    // SUPPRIMER UN PRODUIT
-                    case "delet":
-
-                        //array_ 
-                        header("Location: recap.php");
-                        break;
-                        
-                    // VIDER LE PANIER
-                    case "clear":
-
-                        // Vider l'array de produit en session
-                        unset($_SESSION["products"]);
-                        header("Location: recap.php");
-                        die();                                  // Termine le script courant comme exit().
-                        break;
-
-                    // AUGMENTER LA QUANTITE
-                    case "up-qtt":
-
-                    // DIMINUER LA QUANTITE
-                    case "down-qtt":
-
-                    // AFFICHER LE DETAIL
-                    case "detail":
-
-                }
-
-                
                         $totalGeneral = 0;
                         foreach($_SESSION["products"] as $index => $product) {
                             echo  "<tr>",
-                                        "<td>".$index."</td>",                                                      // Index du produit.
-                                        "<td>".$product["name"]."</td>",                                            // Nom du produit.
-                                        "<td>".number_format($product["price"], 2, ",","&nbsp;")."</td>",           // Prix du produit.
+                                        "<td>".$index."</td>",
+                                        "<td>".$product["name"]."</td>",
+                                        "<td>".number_format($product["price"], 2, ",","&nbsp;")."</td>",
 
-                                        "<td class='quantArea'>
-                                            <input type='image' id='imgMoins' alt='Quantitée moins' src='assets/img/icons/iconMoins.png'>" 
-                                            .$product["qqt"].                                                                               // Quantité de produit.
-                                            "<input type='image' id='imgPlus' alt='Quantitée plus' src='assets/img/icons/iconPlus.png'>
-                                        </td>",
+
+                                        "<td class='quantArea'><input type='image' id='imgMoins' alt='Quantitée moins'
+                                        src='assets/img/icons/iconMoins.png'>".$product["qqt"]."<input type='image' id='imgPlus' alt='Quantitée plus'
+                                        src='assets/img/icons/iconPlus.png'></td>",
                                         
-                                        "<td>".number_format($product["total"], 2, ",","&nbsp;")."</td>",                                   // Sous total par produit.
-                                        "<td>
-                                            <input type='image' id='imgCorbeille' alt='Delet' src='assets/img/icons/corbeille.png'>         
-                                        </td>",
+
+                                        "<td>".number_format($product["total"], 2, ",","&nbsp;")."</td>",
+                                        "<td><input type='image' id='imgCorbeille' alt='Delet'
+                                        src='assets/img/icons/corbeille.png'></td>",
                                     "</tr>";
-                            $totalGeneral += $product["total"];     // Total des produits + le sous total par prduit.
+                            $totalGeneral += $product["total"];
                         }
                         echo "<tr>",
                                     "<td colspan = 2>Nombre de produits : </td>",
@@ -116,9 +67,12 @@
                                 "</tr>",
                             "</tbody>",
                         "</table>";
+
+                function viderPanier() {
+                    unset($_SESSION["products"]);
+                } 
             }
-            
-            header("Location: index.php");
+                               
         ?>
         </main>
     </div>
--- algoPHP_POO_02/appli/traitement.php
+++ algoPHP_POO_02/appli/traitement.php
@@ -1,23 +1,64 @@
 <?php
     session_start();
+?>
 
-    if(isset($_POST["submit"])) {   // Vérifie si la clé submit du tableau $_POST correspond au name de l'input "submit".
-        // Ici on vérifie l'intégrité des valeurs transmises dans le tableau $_POST.
-        $name = filter_input(INPUT_POST, "name", FILTER_UNSAFE_RAW);   // FILTER_SANITIZE_STRING is deprecated (c'était censé supprimer tous caractères spéciaux et balises html, éviter l'injection de code).
-        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $qqt = filter_input(INPUT_POST, "qqt", FILTER_VALIDATE_INT);
+<?php
 
-        // Vérifie si tous les filtres ont fonctionnés.
-        if($name && $price && $qqt) {
-            $product = [
-                "name" => $name,
-                "price" => $price,
-                "qqt" => $qqt,
-                "total" => $price * $qqt
-            ];
+    if(isset($_GET["action"])) {             
 
-            $_SESSION["products"][] = $product;
+        switch($_GET["action"]) {
+
+            // AJOUTER UN PRODUIT
+            case "add":
+
+                if(isset($_POST["submit"])) {   // Vérifie si la clé submit du tableau $_POST correspond au name de l'input "submit".
+
+                    // On vérifie l'intégrité des valeurs transmises dans le tableau $_POST.
+                    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);   // FILTER_SANITIZE_STRING is deprecated (c'était censé supprimer tous caractères spéciaux et balises html, éviter l'injection de code).
+                    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    $qqt = filter_input(INPUT_POST, "qqt", FILTER_VALIDATE_INT);
+
+                    // Vérifie si tous les filtres ont fonctionnés.
+                    if(isset($_GET["action"])) {
+                        $product = [
+                            $action = $_GET["action"],
+                            $name = $_GET["name"],
+                            $price = $_GET["price"],
+                            $qqt = $_GET["qqt"],
+                            $total = $price * $qqt
+                        ];
+                        
+                        
+                    }
+                        array_push($_SESSION["products"] = $product);
+                }
+
+            // SUPPRIMER UN PRODUIT
+            case "delet":
+
+                //array_ 
+                header("Location: recap.php");
+                break;
+                
+            // VIDER LE PANIER
+            case "clear":
+
+                // Vider l'array de produit en session
+                unset($_SESSION["products"]);
+                header("Location: recap.php");
+                die();                                  // Termine le script courant comme exit().
+                break;
+
+            // AUGMENTER LA QUANTITE
+            case "up-qtt":
+
+            // DIMINUER LA QUANTITE
+            case "down-qtt":
+
+            // AFFICHER LE DETAIL
+            case "detail":
         }
+        
     }
     header("Location:recap.php");   // Autrement retourne à la page index de toutes
 ?>
