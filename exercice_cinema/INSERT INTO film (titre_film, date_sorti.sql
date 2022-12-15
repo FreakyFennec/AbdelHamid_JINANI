00b4cb264eb092bd4f883659609c89998c901b2d
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-12-15 11:11:18 +0100

    première requête

---
+++ exercice_cinema/INSERT INTO film (titre_film, date_sorti.sql
@@ -0,0 +1,10 @@
+INSERT INTO film (titre_film, date_sortie_fr, duree_film, synopsis_film, affiche_film, note_film, id_realisateur)
+VALUES ('', '', '', '', '', '', '');
+
+INSERT INTO genre (type_genre_film)
+VALUES ('');
+
+INSERT INTO role (nom_role)
+VALUES (''),
+(''),
+('');
---
+++ exercice_cinema/requetes_exo_cinema.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM film
+WHERE id_film = 1
