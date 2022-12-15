0bf6b19bf018b986fc5e51fd434970f37791c262
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-12-15 16:15:21 +0100

    liste de films, durée sup 135, liste films d'un réal.

--- exercice_cinema/INSERT INTO film (titre_film, date_sorti.sql
+++ exercice_cinema/INSERT INTO film (titre_film, date_sorti.sql
@@ -1,3 +1,6 @@
+INSERT INTO personne (nom_personne, prenom_personne, date_naiss_personne, lieu_naiss_personne, genre_personne)
+VALUES ('', '', '', '', '');
+
 INSERT INTO film (titre_film, date_sortie_fr, duree_film, synopsis_film, affiche_film, note_film, id_realisateur)
 VALUES ('', '', '', '', '', '', '');
 
@@ -8,3 +11,13 @@ INSERT INTO role (nom_role)
 VALUES (''),
 (''),
 ('');
+
+/* Pour modifier une foreign_key */
+alter table T2 add constraint fk_T2_T1  foreign key(T2.C1)  references T1(T1.C1) 
+on update cascade;
+ALTER TABLE film 
+ADD CONSTRAINT fk_film
+FOREIGN KEY ()
+REFERENCES ()
+ON UPDATE CASCADE;
+
--- exercice_cinema/requetes_exo_cinema.sql
+++ exercice_cinema/requetes_exo_cinema.sql
@@ -1,3 +1,53 @@
-SELECT *
-FROM film
-WHERE id_film = 1
+/*== a ==*/
+/*== Afficher infos film ==*/
+SELECT id_film, titre_film, DATE_FORMAT(date_sortie_fr, "%d/%m/%Y") AS date_sortie_fr, CONCAT(p.prenom_personne,' ', p.nom_personne)
+FROM film f
+INNER JOIN realisateur r
+ON f.id_realisateur = r.id_realisateur
+INNER JOIN personne p
+ON p.id_personne = r.id_personne
+WHERE id_film = 1;
+
+/*== b ==*/
+/*== Films dont la durée excède 2h15 classés par durée (DESC) ==*/
+SELECT titre_film, duree_film
+FROM film 
+WHERE duree_film > 135
+ORDER BY duree_film DESC;
+
+/*== c ==*/
+/*== Liste des films d’un réalisateur (en précisant l’année de sortie) ==*/
+SELECT titre_film, date_sortie_fr, CONCAT(p.prenom_personne,' ', p.nom_personne)
+FROM film f
+INNER JOIN realisateur r
+ON f.id_realisateur = r.id_realisateur
+INNER JOIN personne p
+ON p.id_personne = r.id_personne
+WHERE r.id_realisateur = 4;
+
+/*== d ==*/
+/*==  ==*/
+
+/*== e ==*/
+/*==  ==*/
+
+/*== f ==*/
+/*==  ==*/
+
+/*== g ==*/
+/*==  ==*/
+
+/*== h ==*/
+/*==  ==*/
+
+/*== i ==*/
+/*==  ==*/
+
+/*== j ==*/
+/*==  ==*/
+
+/*== k ==*/
+/*==  ==*/
+
+/*== l ==*/
+/*==  ==*/
