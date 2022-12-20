3ad225665f4e54147bf453683f0c144f91552388
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-12-20 19:17:10 +0100

    commented test_connection

--- exercice_cinema/connection_bdd/test_connection.php
+++ exercice_cinema/connection_bdd/test_connection.php
@@ -4,7 +4,8 @@
         $mysqlConnection = new PDO(
             'mysql:host=localhost;dbname=cinema_hamid;charset=utf8',
             'root',
-            ''
+            '',
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION], // Affiche les requêtes qui comportent des erreurs.
         );
     }
     catch (Exception $e) {
@@ -15,16 +16,16 @@
     // Si pas d'erreur on continue
 
     // Récupére le contenu de la table film
-    $sqlQuery = 'SELECT * FROM film';
-    $filmStatement = $mysqlConnection->prepare($sqlQuery);
+    $sqlQuery = 'SELECT * FROM film';                       // Variable qui contient la requête sql.
+    $filmStatement = $mysqlConnection->prepare($sqlQuery);  // On prépare la requête (plus de sécurité).
 
     $filmStatement->execute();
-    $films = $filmStatement->fetchAll();
+    $films = $filmStatement->fetchAll();                    // Va chercher les éléments de la requête.
 
     // On fait un boucle foreach() pour afficher les films.
     foreach ($films as $film) {
         ?>
-        <p><?php echo $film['titre_film']; ?></p>
+        <p><?php echo $film['titre_film']; ?></p>           <!-- Affiche les titre des films -->
         <?php
     }
 ?>
