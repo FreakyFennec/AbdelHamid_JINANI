9dd575a472fb0ffe144b2315a6de6b8ebb092c66
AbdelHamid_JINANI <ahjinani@gmail.com>
2022-12-28 10:51:48 +0100

    created listFilms.php

--- exercice_cinema_mvc/view/listFilms.php
+++ exercice_cinema_mvc/view/listFilms.php
@@ -1,4 +1,28 @@
 <?php ob_start(); ?>
 
 <p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount() ?> films</p>
-<p class="uk-table uk-table-"></p>
+
+<table class="uk-table uk-table-striped">
+    <thead>
+        <tr>
+            <th>Titre</th>
+            <th>Année sortie</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach($requete->fetch_all() as $film) { ?>
+                <tr>
+                    <td><?= $film["titre_film"] ?></td>
+                    <td><?= $film["date_sortie_fr"] ?></td>
+                </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+<?php
+
+    $titre = "Liste des films";
+    $titre_secondaire = "Liste des films";
+    $contenu = ob_get_clean();
+    require "view/template.php";
